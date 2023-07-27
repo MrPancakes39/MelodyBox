5e06377f0a18d51af9c8957b40dcb3ab0ab89292
MrPancakes39 <salmannhassan39@gmail.com>
2023-07-27 09:52:15 +0300

    Little Refactor and Removal Work
    - Renamed Structs.
    - Extracted Code to Function.
    - Removed Compare For Best URL.
    - Moved Base API to it's own constant.

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,11 @@
 const USER_AGENT: &str = "Mozilla/5.0 (X11; Linux x86_64; rv:109.0) Gecko/20100101 Firefox/115.0";
+const API_BASE: &str = "https://piped-api.privacy.com.de";
 
 use serde::Deserialize;
 
 #[derive(Debug, Deserialize)]
 #[serde(rename_all = "camelCase")]
-struct Stream {
+struct AudioStream {
     pub url: String,
     pub format: String,
     pub quality: String,
@@ -12,42 +13,24 @@ struct Stream {
 
 #[derive(Debug, Deserialize)]
 #[serde(rename_all = "camelCase")]
-struct JSONRes {
-    audio_streams: Vec<Stream>,
+struct PipedResponse {
+    audio_streams: Vec<AudioStream>,
 }
 
-#[tokio::main]
-async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let url = "https://piped-api.privacy.com.de/streams/HoBGWhapaho";
+async fn download_song(video_id: &str) -> Result<(), Box<dyn std::error::Error>> {
+    let url = format!("{API_BASE}/streams/{video_id}");
     let client = reqwest::Client::new();
     let resp = client
         .get(url)
         .header("User-Agent", USER_AGENT)
         .send()
         .await?;
-    let json = resp.json::<JSONRes>().await?;
-    let best_audio = json
-        .audio_streams
-        .iter()
-        .filter(|stream| stream.format == "M4A")
-        .max_by(|s1, s2| {
-            let n1 = s1
-                .quality
-                .split(" ")
-                .next()
-                .unwrap()
-                .parse::<i32>()
-                .unwrap();
-            let n2 = s2
-                .quality
-                .split(" ")
-                .next()
-                .unwrap()
-                .parse::<i32>()
-                .unwrap();
-            n1.cmp(&n2)
-        });
-    let best_url = &best_audio.unwrap().url;
-    println!("{:#?}", best_url);
+    let json = resp.json::<PipedResponse>().await?;
+    println!("{:#?}", json);
     Ok(())
+}
+
+#[tokio::main]
+async fn main() {
+    download_song("HoBGWhapaho").await.unwrap();
 }
