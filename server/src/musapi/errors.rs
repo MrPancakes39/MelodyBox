698f4bf61060a0e6002149e36fb388f5a5e2fb9a
MrPancakes39 <salmannhassan39@gmail.com>
2023-09-06 00:31:14 +0300

    Implemented IntoResponse trait for RequestorError

--- server/src/musapi/errors.rs
+++ server/src/musapi/errors.rs
@@ -1,3 +1,6 @@
+use axum::response::{IntoResponse, Response};
+use reqwest::StatusCode;
+
 #[derive(Debug, thiserror::Error)]
 pub enum RequestorError {
     #[error("There was an Error in doing the Request")]
@@ -15,5 +18,22 @@ pub enum RequestorError {
 impl From<reqwest::Error> for RequestorError {
     fn from(error: reqwest::Error) -> Self {
         Self::RequestError(error)
+    }
+}
+
+impl IntoResponse for RequestorError {
+    fn into_response(self) -> Response {
+        log::error!("ERROR: {:?}", self);
+        let message = match self {
+            Self::RequestError(_) | Self::ParseError => None,
+            Self::ApiError(_) | Self::StreamError => {
+                Some("Couldn't get song source from Youtube Music.")
+            }
+            Self::DownloadFailed => Some("Failed to download song from Youtube Music."),
+        };
+        match message {
+            None => StatusCode::INTERNAL_SERVER_ERROR.into_response(),
+            Some(msg) => (StatusCode::INTERNAL_SERVER_ERROR, msg).into_response(),
+        }
     }
 }
--- server/src/router.rs
+++ server/src/router.rs
@@ -2,29 +2,21 @@ use once_cell::sync::Lazy;
 use reqwest::StatusCode;
 
 use crate::musapi::MusicApiClient;
-use axum::response::IntoResponse;
-use axum::{extract::Path, routing::get, Json, Router};
+use axum::{extract::Path, response::IntoResponse, routing::get, Json, Router};
 
 const CLIENT: Lazy<MusicApiClient> = Lazy::new(|| MusicApiClient::new());
 
 async fn info_handler(Path(sid): Path<String>) -> impl IntoResponse {
     log::debug!("SongID = {:?}", &sid);
     match CLIENT.validate_video_id(&sid).await {
-        Err(_) => return (StatusCode::INTERNAL_SERVER_ERROR).into_response(),
+        Err(err) => return err.into_response(),
         Ok(false) => {
             return (StatusCode::BAD_REQUEST, "The id provided is not valid.").into_response()
         }
-        Ok(true) => {}
+        _ => {}
     };
     let info = match CLIENT.get_track_info(&sid).await {
-        Err(err) => {
-            log::error!("ERROR: FAILED TO FETCH INFO {:?}", err);
-            return (
-                StatusCode::INTERNAL_SERVER_ERROR,
-                "Failed to fetch info from Youtube API",
-            )
-                .into_response();
-        }
+        Err(err) => return err.into_response(),
         Ok(info) => info,
     };
     (StatusCode::OK, Json(info)).into_response()
@@ -33,7 +25,7 @@ async fn info_handler(Path(sid): Path<String>) -> impl IntoResponse {
 async fn lyrics_handler(Path(lid): Path<String>) -> impl IntoResponse {
     log::debug!("LyricsID = {:?}", &lid);
     let lyrics = match CLIENT.get_lyrics(&lid).await {
-        Err(_) => return (StatusCode::INTERNAL_SERVER_ERROR).into_response(),
+        Err(err) => return err.into_response(),
         Ok(lyrics) => lyrics,
     };
     (StatusCode::OK, Json(lyrics)).into_response()
