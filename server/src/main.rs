cc6d6afd99a9da553d5dcb50cf91406d68074a82
MrPancakes39 <salmannhassan39@gmail.com>
2023-09-05 19:38:45 +0300

    Cleaned up main.rs

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,33 +1,25 @@
-mod musapi;
+// mod musapi;
 mod prestart;
 
-use axum::{routing::get, Router};
+use axum::{response::Html, routing::get, Router};
 use std::net::SocketAddr;
-
-use musapi::MusicApiClient;
 
 #[tokio::main]
 async fn main() -> color_eyre::Result<()> {
     color_eyre::install()?;
     prestart::prechecks()?;
 
-    let client = MusicApiClient::new();
-    println!("{:?}", client);
-    // let info = client.get_track_info("FJX0JPXD2nM").await?;
-    // dbg!(&info);
-    // let lyrics = client.get_lyrics(&info).await?;
-    // dbg!(&lyrics);
-    // let path = client.download_song(&info.video_id).await?;
-    // println!("Path: {path:#}");
+    let app = Router::new().route("/", get(hello));
 
-    let app = Router::new().route("/", get(|| async { "Hello, world!" }));
-
-    let addr: SocketAddr = "[::]:3000".parse().unwrap();
-    println!("listening on {}", &addr);
-
+    let addr: SocketAddr = "[::]:3000".parse()?;
+    println!("listening on PORT {}", &addr.port());
     axum::Server::bind(&addr)
         .serve(app.into_make_service())
         .await?;
 
     Ok(())
+}
+
+async fn hello() -> Html<&'static str> {
+    Html("<h1>Hello, world</h1>")
 }
